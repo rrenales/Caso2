641cb86fcfbc037f50d35e5cd2730153a1ea8075
Ruben Casals Renales <renales45@gmail.com>
2021-06-27 17:12:33 -0400

    modificado:     deploy.sh 	modificado:     k8s.yaml 	renombrado:     roles/pre-k8s/files/kubernetes.repo -> roles/k8s/files/kubernetes.repo 	modificado:     roles/k8s/tasks/01-confmaster.yaml 	modificado:     roles/pre-k8s/tasks/01-resolv.yaml 	nuevo archivo:  roles/workers/readme.md 	nuevo archivo:  roles/workers/tasks/01-confworkers.yaml 	nuevo archivo:  roles/workers/tasks/main.yaml 	nuevo archivo:  workers.yaml

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,10 +3,16 @@
 #Script para lanzar todos los palybooks de ansible.
 
 # Lanzamos el playbook de tareas previas de configuración para todos los hosts 
-ansible-playbook -i hosts pre.yml
+ansible-playbook -i hosts pre.yaml
 
 # Lanzamos el playbook de configuración e instalación del servidor nfs.
-ansible-playbook -i hosts -l nfs nfs.yml
+ansible-playbook -i hosts -l nfs nfs.yaml
 
 # Lanzamos el playbook de configuración e instalación de kubernetes para todos los hosts excepto nfs.
-ansible-playbook -i hosts -l workers,master k8s.yaml pre.yml
+ansible-playbook -i hosts -l workers,master pre-k8s.yaml
+
+# Lanzamos el playbook de configuración  de kubernetes para el nodo master.
+ansible-playbook -i hosts -l master k8s.yaml 
+
+# Lanzamos el playbook de configuración  de kubernetes para los workers.
+ansible-playbook -i hosts -l master workers.yaml 
