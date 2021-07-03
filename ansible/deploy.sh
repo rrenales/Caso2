d7ffab68e305a74e4bc445e16837b2bd985e2f4e
rrenales <renales45@gmail.com>
2021-07-03 13:25:08 +0000

    new file:   ansible/README.md 	new file:   ansible/deploy.sh 	new file:   ansible/hosts 	new file:   ansible/k8s-master.yaml 	new file:   ansible/k8s.yaml 	new file:   ansible/nfs.yaml 	new file:   ansible/pre-k8s.yaml 	new file:   ansible/pre.yaml 	new file:   ansible/roles/config/readme.md 	new file:   ansible/roles/config/tasks/01-timezone.yaml 	new file:   ansible/roles/config/tasks/02-chrony.yaml 	new file:   ansible/roles/config/tasks/03-install.yaml 	new file:   ansible/roles/config/tasks/main.yaml 	new file:   ansible/roles/k8s-master/readme.md 	new file:   ansible/roles/k8s-master/tasks/01-ingress.yaml 	new file:   ansible/roles/k8s-master/tasks/main.yaml 	new file:   ansible/roles/k8s/.txt 	new file:   ansible/roles/k8s/files/kubernetes.repo 	new file:   ansible/roles/k8s/tasks/01-confmaster.yaml 	new file:   ansible/roles/k8s/tasks/main.yaml 	new file:   ansible/roles/nfs/tasks/01-install.yaml 	new file:   ansible/roles/nfs/tasks/02-configure.yaml 	new file:   ansible/roles/nfs/tasks/main.yaml 	new file:   ansible/roles/nfs/templates/exports.j2 	new file:   ansible/roles/pre-k8s/files/hosts 	new file:   ansible/roles/pre-k8s/files/k8s.conf 	new file:   ansible/roles/pre-k8s/readme.md 	new file:   ansible/roles/pre-k8s/tasks/01-resolv.yaml 	new file:   ansible/roles/pre-k8s/tasks/02-swap.yaml 	new file:   ansible/roles/pre-k8s/tasks/03-install.yaml 	new file:   ansible/roles/pre-k8s/tasks/main.yaml 	new file:   ansible/roles/pre-k8s/templates/hosts.j2 	new file:   ansible/roles/workers/readme.md 	new file:   ansible/roles/workers/tasks/01-confworkers.yaml 	new file:   ansible/roles/workers/tasks/main.yaml 	new file:   ansible/workers.yaml

---
+++ ansible/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Script para lanzar todos los palybooks de ansible.
+
+# Lanzamos el playbook de tareas previas de configuración para todos los hosts 
+ansible-playbook -i hosts pre.yaml
+
+# Lanzamos el playbook de configuración e instalación del servidor nfs.
+ansible-playbook -i hosts -l nfs nfs.yaml
+
+# Lanzamos el playbook de configuración e instalación de kubernetes para todos los hosts excepto nfs.
+ansible-playbook -i hosts -l workers,master pre-k8s.yaml
+
+# Lanzamos el playbook de configuración  de kubernetes para el nodo master.
+ansible-playbook -i hosts -l master k8s.yaml 
+
+# Lanzamos el playbook de configuración  de kubernetes para los workers.
+ansible-playbook -i hosts -l workers workers.yaml
+
+# Lanzamos el playbook de configuración  de ingress i creacion usuario kubeadmin en el master
+ansible-playbook -i hosts -l master k8s-master.yaml
